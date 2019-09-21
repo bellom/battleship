6de4fd9bc883b8936d1b9cc63297eddb4dde5af0
Saheed <sdrfngg640@gmail.com>
2019-09-21 16:02:08 +0100

    Update eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "airbnb base",
+    "extends": "airbnb-base",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
