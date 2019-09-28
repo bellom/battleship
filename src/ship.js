0bcfbda8b19fef2c129cff76cf9aa78e8e0d1900
Saheed <sdrfngg640@gmail.com>
2019-09-28 06:43:27 +0100

    Add ship tests and functions

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,18 @@
-const ship = () => { };
+const ship = (size) => {
+  const coordinates = [];
+  const hits = [];
+
+  const hit = (coordinate) => hits.push(coordinate);
+
+  const isSunk = () => size === hits.length;
+
+  return {
+    size,
+    hit,
+    hits,
+    isSunk,
+    coordinates,
+  };
+};
 
 export default ship;
